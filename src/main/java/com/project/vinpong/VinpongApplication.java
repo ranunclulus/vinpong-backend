67385c8eb40698b324440916ab0eb5c0aeb73211
Huisu <87214089+ranunclulus@users.noreply.github.com>
2024-09-03 16:39:15 +0900

    ✨ feat: temp Member Entity Create

--- src/main/java/com/project/vinpong/VinpongApplication.java
+++ src/main/java/com/project/vinpong/VinpongApplication.java
@@ -2,8 +2,10 @@ package com.project.vinpong;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class VinpongApplication {
 
 	public static void main(String[] args) {
