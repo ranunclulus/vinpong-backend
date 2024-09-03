eab42b798b2a56ff201ea37242d745719fbd6df3
Huisu <87214089+ranunclulus@users.noreply.github.com>
2024-09-03 17:00:43 +0900

    ✨ feat: Error / Base ReasonDto & Status 수정 후 응답 반환하도록

--- src/main/java/com/project/vinpong/apiPayload/code/ErrorReasonDto.java
+++ src/main/java/com/project/vinpong/apiPayload/code/ErrorReasonDto.java
@@ -1,4 +1,19 @@
 package com.project.vinpong.apiPayload.code;
 
+import lombok.Builder;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@Builder
 public class ErrorReasonDto {
+    private final HttpStatus httpStatus;
+    private final boolean isSuccess;
+    private final String code;
+    private final String message;
+
+    public boolean getIsSuccess() {
+        return this.isSuccess;
+    }
+
 }
--- src/main/java/com/project/vinpong/apiPayload/code/status/SuccessStatus.java
+++ src/main/java/com/project/vinpong/apiPayload/code/status/SuccessStatus.java
@@ -2,17 +2,35 @@ package com.project.vinpong.apiPayload.code.status;
 
 import com.project.vinpong.apiPayload.code.BaseCode;
 import com.project.vinpong.apiPayload.code.ReasonDto;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
 
+@Getter
+@AllArgsConstructor
 public enum SuccessStatus implements BaseCode {
-    ;
+    _OK(HttpStatus.OK, "COMMON200", "성공입니다.");
+
+    private final HttpStatus httpStatus;
+    private final String code;
+    private final String message;
 
     @Override
     public ReasonDto getReason() {
-        return null;
+        return ReasonDto.builder()
+                .message(message)
+                .code(code)
+                .isSuccess(true)
+                .build();
     }
 
     @Override
     public ReasonDto getReasonHttpStatus() {
-        return null;
+        return ReasonDto.builder()
+                .message(message)
+                .code(code)
+                .isSuccess(true)
+                .httpStatus(httpStatus)
+                .build();
     }
 }
