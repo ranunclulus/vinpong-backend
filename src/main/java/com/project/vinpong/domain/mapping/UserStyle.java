c1f8a099f47db2906598cfa3a76763d228c1ff03
Huisu <87214089+ranunclulus@users.noreply.github.com>
2024-09-04 14:23:47 +0900

    ✨ feat: mapping Entity

---
+++ src/main/java/com/project/vinpong/domain/mapping/UserStyle.java
@@ -0,0 +1,19 @@
+package com.project.vinpong.domain.mapping;
+
+import com.project.vinpong.domain.common.BaseEntity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class UserStyle extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long userStyleId;
+}
