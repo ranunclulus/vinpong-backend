41b7e21e858b5258ec27b7a0961165d53e42c9e7
Huisu <87214089+ranunclulus@users.noreply.github.com>
2024-09-06 15:24:51 +0900

    ✨ feat: UserController 임의 작성

--- src/main/java/com/project/vinpong/controller/UserController.java
+++ src/main/java/com/project/vinpong/controller/UserController.java
@@ -1,6 +1,9 @@
 package com.project.vinpong.controller;
 
 import com.project.vinpong.apiPayload.ApiResponse;
+import com.project.vinpong.converter.UserConverter;
+import com.project.vinpong.domain.User;
+import com.project.vinpong.service.UserService.UserCommandService;
 import com.project.vinpong.web.dto.UserRequestDTO;
 import com.project.vinpong.web.dto.UserResponseDTO;
 import jakarta.validation.Valid;
@@ -15,10 +18,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RequestMapping("/v1/users")
 public class UserController {
+    private final UserCommandService userCommandService;
 
 
     @PostMapping("/signup/general")
     public ApiResponse<UserResponseDTO.JoinResultDTO> join(@RequestBody @Valid UserRequestDTO.JoinDTO request) {
+        User user = userCommandService.joinUser(request);
         return null;
     }
 }
